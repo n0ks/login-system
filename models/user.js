070afa061240ec69832fde9503ae2fd05e53aa70
n0ks <rodrigocmp29@gmail.com>
2017-10-16 23:20:12 -0200

    Member Registration

---
+++ models/user.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/nodeauth');
+var db = mongoose.connection;
+
+//User scheme
+
+var UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        index: true
+    },
+    password: {
+        type: String,
+
+    },
+    email: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    profileImage: {
+        type: String
+    }
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+module.exports.createUser =  function(newUser, callback){
+    newUser.save(callback);
+    
+}
