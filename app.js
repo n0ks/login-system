23e4b99a75e8466f38d7b7f7e84cfd903b46d524
n0ks <rodrigocmp29@gmail.com>
2017-11-02 16:37:05 -0200

    bcrypt

--- app.js
+++ app.js
@@ -24,32 +24,34 @@ var app = express();
 app.set('views', path.join(__dirname, '/views'));
 
 app.set('view engine', 'jade');
-app.use(multer({dest:'./uploads/'}).single('photo'));;
+app.use(multer({
+	dest: './uploads/'
+}).single('photo'));;
 
 app.use(session({
-    secret: 'secret',
-    saveUninitialized: true,
-    resave: true
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(expressValidator({
-    errorFormatter: function (param, msg, value) {
-        var namespace = param.split('.')
-            , root = namespace.shift()
-            , formParam = root;
-        while (namespace.length) {
-            formParam += '[' + namespace.shift() + ']';
-        }
-        return {
-            param: formParam,
-            msg: msg,
-            value: value
-        }
+	errorFormatter: function (param, msg, value) {
+		var namespace = param.split('.'),
+			root = namespace.shift(),
+			formParam = root;
+		while (namespace.length) {
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param: formParam,
+			msg: msg,
+			value: value
+		}
 
-    }
+	}
 }));
 
 // uncomment after placing your favicon in /public
@@ -57,14 +59,14 @@ app.use(expressValidator({
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+	extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
 app.use(function (req, res, next) {
-    res.locals.messages = require('express-messages')(req,res);
-    next();
+	res.locals.messages = require('express-messages')(req, res);
+	next();
 });
 
 app.use('/', index);
@@ -72,20 +74,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
